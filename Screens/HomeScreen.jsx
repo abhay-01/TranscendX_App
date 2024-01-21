5f9a9b1f74c8ed49ad6939093661dc4db759769b
abhay-01 <pratapsinghabhay999@gmail.com>
2024-01-21 15:47:16 +0530

    changes in screen

--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { usePushNotifications } from './usePushNotifications'
 
 export default function HomeScreen() {
+  const {expoPushToken} = usePushNotifications();
+
+  console.log("expoPushToken: ", expoPushToken);
   return (
     <View>
         <Text>HomeScreen</Text>
